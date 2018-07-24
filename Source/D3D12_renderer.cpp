843b581847162e8565c1a8a864b3fe3128b5f7b1
jpmmaia <jpmmaia@gmail.com>
2018-07-24 20:29:34 +0300

    Creating swap chain.

--- Source/D3D12_renderer.cpp
+++ Source/D3D12_renderer.cpp
@@ -22,13 +22,57 @@ namespace Mythology
 		m_factory{ create_factory({}) },
 		m_adapter{ select_adapter(*m_factory, true) },
 		m_device{ create_dxr_device(*m_adapter, D3D_FEATURE_LEVEL_12_0) },
-		m_render_command_queue { create_command_queue(*m_device, D3D12_COMMAND_LIST_TYPE_DIRECT, 0, D3D12_COMMAND_QUEUE_FLAG_NONE, 0) },
+		m_direct_command_queue{ create_command_queue(*m_device, D3D12_COMMAND_LIST_TYPE_DIRECT, 0, D3D12_COMMAND_QUEUE_FLAG_NONE, 0) },
 		m_command_allocator{ create_command_allocator(*m_device, D3D12_COMMAND_LIST_TYPE_DIRECT) },
 		m_command_list{ create_closed_graphics_command_list(*m_device, 0, D3D12_COMMAND_LIST_TYPE_DIRECT, *m_command_allocator, nullptr) },
 		m_rtv_descriptor_heap{ create_descriptor_heap(*m_device, D3D12_DESCRIPTOR_HEAP_TYPE_RTV, static_cast<UINT>(m_pipeline_length), D3D12_DESCRIPTOR_HEAP_FLAG_NONE, 0) },
 		m_fence_value{ 0 },
 		m_fence{ create_fence(*m_device, m_fence_value, D3D12_FENCE_FLAG_NONE) },
-		m_fence_event{ ::CreateEvent(nullptr, false, false, nullptr) }
+		m_fence_event{ ::CreateEvent(nullptr, false, false, nullptr) },
+		m_swap_chain{}
 	{
+	}
+
+	namespace
+	{
+		winrt::com_ptr<IDXGISwapChain1> create_swap_chain(IDXGIFactory2& factory, IUnknown& direct_command_queue, IUnknown& window, UINT buffer_count)
+		{
+			DXGI_SWAP_CHAIN_DESC1 description;
+			description.Width = 0;
+			description.Height = 0;
+			description.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+			description.Stereo = true;
+			description.SampleDesc.Count = 1;
+			description.SampleDesc.Quality = 0;
+			description.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+			description.BufferCount = buffer_count;
+			description.Scaling = DXGI_SCALING_NONE;
+			description.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+			description.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
+			description.Flags = {};
+
+			winrt::com_ptr<IDXGISwapChain1> swap_chain;
+			winrt::check_hresult(
+				factory.CreateSwapChainForCoreWindow(
+					&direct_command_queue,
+					&window,
+					&description,
+					nullptr,
+					swap_chain.put()
+				)
+			);
+
+			return swap_chain;
+		}
+	}
+
+	void D3D12_renderer::window(IUnknown& window)
+	{
+		m_swap_chain = create_swap_chain(
+			*m_factory, 
+			*m_direct_command_queue, 
+			window, 
+			static_cast<UINT>(m_pipeline_length)
+		);
 	}
 }
--- Source/D3D12_renderer.h
+++ Source/D3D12_renderer.h
@@ -16,19 +16,22 @@ namespace Mythology
 
 		D3D12_renderer();
 
+		void window(IUnknown& window);
+
 	private:
 
 		std::size_t const m_pipeline_length;
 		winrt::com_ptr<IDXGIFactory6> m_factory;
 		winrt::com_ptr<IDXGIAdapter> m_adapter;
 		winrt::com_ptr<ID3D12Device> m_device;
-		winrt::com_ptr<ID3D12CommandQueue> m_render_command_queue;
+		winrt::com_ptr<ID3D12CommandQueue> m_direct_command_queue;
 		winrt::com_ptr<ID3D12CommandAllocator> m_command_allocator;
 		winrt::com_ptr<ID3D12CommandList> m_command_list;
 		winrt::com_ptr<ID3D12DescriptorHeap> m_rtv_descriptor_heap;
 		UINT64 m_fence_value;
 		winrt::com_ptr<ID3D12Fence> m_fence;
 		winrt::handle m_fence_event;
+		winrt::com_ptr<IDXGISwapChain> m_swap_chain;
 
 	};
 }
