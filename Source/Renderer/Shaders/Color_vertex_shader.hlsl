53f82318b12a01c4d51b33d451d06e713bcb55d7
JPMMaia <jpmmaia@gmail.com>
2018-12-30 18:48:50 +0200

    Added instances buffer with world matrix.

--- Source/Renderer/Shaders/Color_vertex_shader.hlsl
+++ Source/Renderer/Shaders/Color_vertex_shader.hlsl
@@ -1,11 +1,17 @@
-struct Input_position_h
+struct World_position
 {
 	float4 value : POSITION;
 };
 
-struct Input_color
+struct Color
 {
 	float4 value : COLOR;
+};
+
+struct Instance_data
+{
+	float4x4 world_matrix : WORLD;
+	uint instance_ID : SV_INSTANCEID;
 };
 
 struct Vertex_shader_output
@@ -14,10 +20,10 @@ struct Vertex_shader_output
     float4 color : COLOR;
 };
 
-Vertex_shader_output main(Input_position_h positionH, Input_color color)
+Vertex_shader_output main(World_position world_position, Color color, Instance_data instance_data)
 {
     Vertex_shader_output output;
-    output.positionH = positionH.value;
+	output.positionH = mul(instance_data.world_matrix, world_position.value);
     output.color = color.value;
 
     return output;
--- Source/Scene.cpp
+++ Source/Scene.cpp
@@ -34,6 +34,11 @@ namespace Maia::Mythology
 		std::vector<std::uint16_t> indices;
 	};
 
+	struct Instance_data
+	{
+		Eigen::Matrix4f world_matrix;
+	};
+
 	namespace
 	{
 		Colored_mesh create_triangle_mesh()
@@ -64,27 +69,27 @@ namespace Maia::Mythology
 
 		void upload_geometry_data(
 			Colored_mesh const& mesh, 
-			D3D12::Geometry_and_instances_buffer& buffer,
-			ID3D12Device& device,
-			ID3D12Resource& upload_buffer, UINT64& upload_buffer_offset, // TODO create a struct for both
+			D3D12::Geometry_and_instances_buffer& buffer, UINT64 const base_buffer_offset,
+			ID3D12Resource& upload_buffer, UINT64 const upload_buffer_offset, // TODO create a struct for both
 			ID3D12GraphicsCommandList& command_list
 		)
 		{
 			using namespace Maia::Renderer::D3D12;
 
 			Mapped_memory mapped_memory{ upload_buffer, 0, {} };
-			UINT64 offset = 0;
+
+			UINT64 buffer_offset{ base_buffer_offset };
 
 			{
 				gsl::span<Position const> const positions{ mesh.vertices.positions };
 				upload_buffer_data(
 					mapped_memory,
 					command_list,
-					*buffer.value, offset,
-					upload_buffer, upload_buffer_offset + offset,
+					*buffer.value, buffer_offset,
+					upload_buffer, upload_buffer_offset + buffer_offset,
 					positions
 				);
-				offset += positions.size_bytes();
+				buffer_offset += positions.size_bytes();
 			}
 
 			{
@@ -92,11 +97,11 @@ namespace Maia::Mythology
 				upload_buffer_data(
 					mapped_memory,
 					command_list,
-					*buffer.value, offset,
-					upload_buffer, upload_buffer_offset + offset,
+					*buffer.value, buffer_offset,
+					upload_buffer, upload_buffer_offset + buffer_offset,
 					colors
 				);
-				offset += colors.size_bytes();
+				buffer_offset += colors.size_bytes();
 			}
 
 			{
@@ -104,19 +109,40 @@ namespace Maia::Mythology
 				upload_buffer_data(
 					mapped_memory,
 					command_list,
-					*buffer.value, offset,
-					upload_buffer, upload_buffer_offset + offset,
+					*buffer.value, buffer_offset,
+					upload_buffer, upload_buffer_offset + buffer_offset,
 					indices
 				);
-				offset += indices.size_bytes();
+				buffer_offset += indices.size_bytes();
 			}
 		}
 
-		void upload_instances_data(D3D12::Geometry_and_instances_buffer& buffer)
+		void upload_instances_data(
+			D3D12::Geometry_and_instances_buffer& buffer, UINT64 const base_buffer_offset,
+			ID3D12Resource& upload_buffer, UINT64 const upload_buffer_offset, // TODO create a struct for both
+			ID3D12GraphicsCommandList& command_list,
+			gsl::span<Instance_data> instances_data)
 		{
+			using namespace Maia::Renderer::D3D12;
+
+			upload_buffer_data(
+				command_list,
+				*buffer.value, base_buffer_offset,
+				upload_buffer, upload_buffer_offset,
+				instances_data
+			);
 		}
 
-
+		std::vector<Instance_data> create_instances_data()
+		{
+			Eigen::Matrix4f world_matrix;
+			world_matrix <<
+				1.0f, 0.0f, 0.0f, 0.0f,
+				0.0f, 1.0f, 0.0f, 0.0f,
+				0.0f, 0.0f, 1.0f, 0.0f,
+				0.0f, 0.0f, 0.0f, 1.0f;
+			return { { world_matrix } };
+		}
 	}
 
 	Maia::Mythology::D3D12::Scene_resources load(Maia::GameEngine::Entity_manager& entity_manager, Maia::Mythology::D3D12::Render_resources const& render_resources)
@@ -125,7 +151,7 @@ namespace Maia::Mythology
 		ID3D12Heap& heap = *render_resources.buffers_heap;
 		UINT64 heap_offset = render_resources.buffers_heap_offset;
 		ID3D12Resource& upload_buffer = *render_resources.upload_buffer;
-		UINT64 upload_buffer_offset = render_resources.upload_buffer_offset;
+		UINT64 const upload_buffer_offset = render_resources.upload_buffer_offset;
 		ID3D12GraphicsCommandList& command_list = *render_resources.command_list;
 
 		using namespace Maia::Renderer::D3D12;
@@ -134,13 +160,15 @@ namespace Maia::Mythology
 
 		{
 			Colored_mesh const mesh = create_triangle_mesh();
+			std::vector<Instance_data> instances = create_instances_data();
 
+			UINT64 positions_size_bytes = mesh.vertices.positions.size() * sizeof(decltype(mesh.vertices.positions)::value_type);
+			UINT64 color_size_bytes = mesh.vertices.colors.size() * sizeof(decltype(mesh.vertices.colors)::value_type);
+			UINT64 indices_size_bytes = mesh.indices.size() * sizeof(decltype(mesh.indices)::value_type);
 			UINT64 const geometry_buffer_width =
-				mesh.vertices.positions.size() * sizeof(decltype(mesh.vertices.positions)::value_type) +
-				mesh.vertices.colors.size() * sizeof(decltype(mesh.vertices.colors)::value_type) +
-				mesh.indices.size() * sizeof(decltype(mesh.indices)::value_type);
-			UINT64 const instances_buffer_width = 0;
-			UINT64 const total_buffer_width = geometry_buffer_width + instances_buffer_width;
+				positions_size_bytes + color_size_bytes + indices_size_bytes;
+			UINT64 const instances_data_size_bytes = sizeof(Instance_data) * instances.size();
+			UINT64 const total_buffer_width = geometry_buffer_width + instances_data_size_bytes;
 
 			D3D12::Geometry_and_instances_buffer geometry_and_instances_buffer = 
 			{
@@ -152,27 +180,24 @@ namespace Maia::Mythology
 				)
 			};
 
-			upload_geometry_data(mesh, geometry_and_instances_buffer, device, upload_buffer, upload_buffer_offset, command_list);
-
-			heap_offset += total_buffer_width;
+			{
+				upload_geometry_data(mesh, geometry_and_instances_buffer, 0, upload_buffer, upload_buffer_offset, command_list);
+				upload_instances_data(geometry_and_instances_buffer, geometry_buffer_width, upload_buffer, upload_buffer_offset + geometry_buffer_width, command_list, instances);
+			}
 
 			{
 				Maia::Mythology::D3D12::Render_primitive render_primitive{};
-
-				UINT64 offset{ 0 };
 
 				{
 					D3D12_VERTEX_BUFFER_VIEW const position_buffer_view = [&]() -> D3D12_VERTEX_BUFFER_VIEW
 					{
 						D3D12_VERTEX_BUFFER_VIEW buffer_view;
-						buffer_view.BufferLocation = geometry_and_instances_buffer.value->GetGPUVirtualAddress() + offset;
+						buffer_view.BufferLocation = geometry_and_instances_buffer.value->GetGPUVirtualAddress();
 
 						using data_type = decltype(mesh.vertices.positions)::value_type;
 						gsl::span<data_type const> const data_view = mesh.vertices.positions;
 						buffer_view.SizeInBytes = static_cast<UINT>(data_view.size_bytes());
 						buffer_view.StrideInBytes = sizeof(data_type);
-
-						offset += data_view.size_bytes();
 
 						return buffer_view;
 					}();
@@ -180,14 +205,27 @@ namespace Maia::Mythology
 					D3D12_VERTEX_BUFFER_VIEW const color_buffer_view = [&]() -> D3D12_VERTEX_BUFFER_VIEW
 					{
 						D3D12_VERTEX_BUFFER_VIEW buffer_view;
-						buffer_view.BufferLocation = geometry_and_instances_buffer.value->GetGPUVirtualAddress() + offset;
+						buffer_view.BufferLocation = geometry_and_instances_buffer.value->GetGPUVirtualAddress() +
+							positions_size_bytes;
 						
 						using data_type = decltype(mesh.vertices.colors)::value_type;
 						gsl::span<data_type const> const data_view = mesh.vertices.colors;
 						buffer_view.SizeInBytes = static_cast<UINT>(data_view.size_bytes());
 						buffer_view.StrideInBytes = sizeof(data_type);
 
-						offset += data_view.size_bytes();
+						return buffer_view;
+					}();
+
+					D3D12_VERTEX_BUFFER_VIEW const instance_buffer_view = [&]() -> D3D12_VERTEX_BUFFER_VIEW
+					{
+						D3D12_VERTEX_BUFFER_VIEW buffer_view;
+						buffer_view.BufferLocation = geometry_and_instances_buffer.value->GetGPUVirtualAddress() + 
+							geometry_buffer_width;
+
+						using data_type = Instance_data;
+						gsl::span<data_type const> const data_view = instances;
+						buffer_view.SizeInBytes = static_cast<UINT>(data_view.size_bytes());
+						buffer_view.StrideInBytes = sizeof(data_type);
 
 						return buffer_view;
 					}();
@@ -195,13 +233,15 @@ namespace Maia::Mythology
 					render_primitive.vertex_buffer_views =
 					{
 						position_buffer_view,
-						color_buffer_view
+						color_buffer_view,
+						instance_buffer_view
 					};
 				}
 
 				{
 					D3D12_INDEX_BUFFER_VIEW index_buffer_view;
-					index_buffer_view.BufferLocation = geometry_and_instances_buffer.value->GetGPUVirtualAddress() + offset;
+					index_buffer_view.BufferLocation = geometry_and_instances_buffer.value->GetGPUVirtualAddress() +
+						positions_size_bytes + color_size_bytes;
 					
 					using data_type = decltype(mesh.indices)::value_type;
 					gsl::span<data_type const> const data_view = mesh.indices;
