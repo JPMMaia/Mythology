38768afd1202b3634b75f384702bd6be58772593
João Maia <jpmmaia@gmail.com>
2019-12-11 20:55:07 +0200

    Add semaphore functions

--- Internal/Renderer/Source/Vulkan/Semaphore.cpp
+++ Internal/Renderer/Source/Vulkan/Semaphore.cpp
@@ -1,6 +1,52 @@
 module maia.renderer.vulkan.semaphore;
 
+import maia.renderer.vulkan.allocation_callbacks;
+import maia.renderer.vulkan.check;
+import maia.renderer.vulkan.device;
+
+import <vulkan/vulkan.h>;
+
+import <optional>;
+import <span>;
+
 namespace Maia::Renderer::Vulkan
 {
+    Semaphore create_semaphore(
+        Device const device,
+        VkSemaphoreCreateFlags const flags,
+        std::optional<Allocation_callbacks> const allocator
+    ) noexcept
+    {
+        VkSemaphoreCreateInfo const create_info
+        {
+            .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
+            .pNext = nullptr,
+            .flags = flags
+        };
 
+        VkSemaphore semaphore;
+        check_result(
+            vkCreateSemaphore(
+                device.value, 
+                &create_info,
+                allocator.has_value() ? &allocator->value : nullptr,
+                &semaphore
+            )
+        );
+
+        return {semaphore};
+    }
+
+    void destroy_semaphore(
+        Device const device,
+        Semaphore const semaphore,
+        std::optional<Allocation_callbacks> const allocator
+    ) noexcept
+    {
+        vkDestroySemaphore(
+            device.value,
+            semaphore.value,
+            allocator.has_value() ? &allocator->value : nullptr
+        );
+    }
 }
--- Internal/Renderer/Source/Vulkan/Semaphore.cxx
+++ Internal/Renderer/Source/Vulkan/Semaphore.cxx
@@ -1,6 +1,12 @@
 export module maia.renderer.vulkan.semaphore;
 
+import maia.renderer.vulkan.allocation_callbacks;
+import maia.renderer.vulkan.device;
+
 import <vulkan/vulkan.h>;
+
+import <optional>;
+import <span>;
 
 namespace Maia::Renderer::Vulkan
 {
@@ -8,4 +14,16 @@ namespace Maia::Renderer::Vulkan
     {
         VkSemaphore value;
     };
+
+    export Semaphore create_semaphore(
+        Device device,
+        VkSemaphoreCreateFlags flags,
+        std::optional<Allocation_callbacks> allocator
+    ) noexcept;
+
+    export void destroy_semaphore(
+        Device device,
+        Semaphore semaphore,
+        std::optional<Allocation_callbacks> allocator
+    ) noexcept;
 }
