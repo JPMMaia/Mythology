8e672168eb808a363b6ff713a022f631db25d435
João Maia <jpmmaia@gmail.com>
2019-12-18 23:01:46 +0200

    Add swapchain

---
+++ Internal/Renderer/Source/Vulkan/Swapchain.cpp
@@ -0,0 +1,185 @@
+module maia.renderer.vulkan.swapchain;
+
+import maia.renderer.vulkan.allocation_callbacks;
+import maia.renderer.vulkan.check;
+import maia.renderer.vulkan.device;
+import maia.renderer.vulkan.fence;
+import maia.renderer.vulkan.image;
+import maia.renderer.vulkan.queue;
+import maia.renderer.vulkan.semaphore;
+import maia.renderer.vulkan.surface;
+
+import <vulkan/vulkan.h>;
+
+import <cstdint>;
+import <memory_resource>;
+import <optional>;
+import <span>;
+import <vector>;
+
+namespace Maia::Renderer::Vulkan
+{
+    Swapchain create_swapchain(
+        Device const device,
+        VkSwapchainCreateFlagsKHR const flags,
+        Min_image_count const min_image_count,
+        Surface const surface,
+        VkFormat const image_format,
+        VkColorSpaceKHR const image_color_space,
+        VkExtent2D const image_extent,
+        Array_layer_count const image_array_layers,
+        VkImageUsageFlags const image_usage,
+        VkSharingMode const image_sharing_mode,
+        std::span<Queue_family_index const> const queue_family_indices,
+        VkSurfaceTransformFlagBitsKHR const pre_transform,
+        VkCompositeAlphaFlagBitsKHR const composite_alpha,
+        VkPresentModeKHR const present_mode,
+        bool const clipped,
+        Swapchain const old_swapchain,
+        std::optional<Allocation_callbacks> const allocator
+    ) noexcept
+    {
+        static_assert(std::is_standard_layout_v<Queue_family_index>, "Must be standard layout so that Queue_family_index and Queue_family_index.value are pointer-interconvertible");
+        static_assert(sizeof(Queue_family_index) == sizeof(uint32_t), "Queue_family_index must only contain uint32_t since using Queue_family_index* as a contiguous array");
+
+        VkSwapchainCreateInfoKHR const create_info
+        {
+            .sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,
+            .pNext = nullptr,
+            .flags = flags,
+            .surface = surface.value,
+            .minImageCount = min_image_count.value,
+            .imageFormat = image_format,
+            .imageColorSpace = image_color_space,
+            .imageExtent = image_extent,
+            .imageArrayLayers = image_array_layers.value,
+            .imageUsage = image_usage,
+            .imageSharingMode = image_sharing_mode,
+            .queueFamilyIndexCount = static_cast<uint32_t>(queue_family_indices.size()),
+            .pQueueFamilyIndices = reinterpret_cast<uint32_t const*>(queue_family_indices.data()),
+            .preTransform = pre_transform,
+            .compositeAlpha = composite_alpha,
+            .presentMode = present_mode,
+            .clipped = clipped,
+            .oldSwapchain = old_swapchain.value,
+        };
+
+        VkSwapchainKHR swapchain = {};
+        check_result(
+            vkCreateSwapchainKHR(
+                device.value,
+                &create_info,
+                allocator.has_value() ? &allocator->value : nullptr,
+                &swapchain
+            )
+        );
+
+        return {swapchain};
+    }
+
+    void destroy_swapchain(
+        Device const device,
+        Swapchain const swapchain,
+        std::optional<Allocation_callbacks> const allocator
+    ) noexcept
+    {
+        vkDestroySwapchainKHR(
+            device.value,
+            swapchain.value,
+            allocator.has_value() ? &allocator->value : nullptr
+        );
+    }
+
+
+    std::pmr::vector<VkImage> get_swapchain_images(
+        Device const device,
+        Swapchain const swapchain,
+        std::pmr::polymorphic_allocator<VkSurfaceFormatKHR> const allocator
+    ) noexcept
+    {
+        std::uint32_t swapchain_image_count = 0;
+        check_result(
+            vkGetSwapchainImagesKHR(
+                device.value,
+                swapchain.value,
+                &swapchain_image_count,
+                nullptr
+            )
+        );
+
+        if (swapchain_image_count > 0)
+        {
+            std::pmr::vector<VkImage> swapchain_images{swapchain_image_count, allocator};
+
+            check_result(
+                vkGetSwapchainImagesKHR(
+                    device.value,
+                    swapchain.value,
+                    &swapchain_image_count,
+                    nullptr
+                )
+            );
+
+            return swapchain_images;  
+        }
+        else 
+        {
+            return {};
+        }
+    }
+
+
+    Swapchain_image_index acquire_next_image(
+        Device const device,
+        Swapchain const swapchain,
+        std::uint64_t const timeout,
+        std::optional<Semaphore> const semaphore,
+        std::optional<Fence> const fence
+    ) noexcept
+    {
+        std::uint32_t swapchain_image_index = 0;
+        check_result(
+            vkAcquireNextImageKHR(
+                device.value,
+                swapchain.value,
+                timeout,
+                semaphore.has_value() ? semaphore->value : VK_NULL_HANDLE,
+                fence.has_value() ? fence->value : VK_NULL_HANDLE,
+                &swapchain_image_index
+            )
+        );
+
+        return {swapchain_image_index};
+    }
+
+    void queue_present(
+        Queue const queue,
+        std::span<VkSemaphore const> const semaphores_to_wait,
+        Swapchain const swapchain,
+        Swapchain_image_index const swapchain_image_index
+    ) noexcept
+    {
+        VkResult result = {};
+
+        VkPresentInfoKHR const present_info
+        {
+            .sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR,
+            .pNext = nullptr,
+            .waitSemaphoreCount = static_cast<uint32_t>(semaphores_to_wait.size()),
+            .pWaitSemaphores = semaphores_to_wait.data(),
+            .swapchainCount = 1,
+            .pSwapchains = &swapchain.value,
+            .pImageIndices = &swapchain_image_index.value,
+            .pResults = &result,
+        };
+
+        check_result(
+            vkQueuePresentKHR(
+                queue.value,
+                &present_info
+            )
+        );
+
+        check_result(result);
+    }
+}
---
+++ Internal/Renderer/Source/Vulkan/Swapchain.cxx
@@ -0,0 +1,85 @@
+export module maia.renderer.vulkan.swapchain;
+
+import maia.renderer.vulkan.allocation_callbacks;
+import maia.renderer.vulkan.device;
+import maia.renderer.vulkan.fence;
+import maia.renderer.vulkan.image;
+import maia.renderer.vulkan.queue;
+import maia.renderer.vulkan.semaphore;
+import maia.renderer.vulkan.surface;
+
+import <vulkan/vulkan.h>;
+
+import <cstdint>;
+import <memory_resource>;
+import <optional>;
+import <span>;
+import <vector>;
+
+namespace Maia::Renderer::Vulkan
+{
+    export struct Swapchain
+    {
+        VkSwapchainKHR value;
+    };
+
+    export struct Min_image_count
+    {
+        std::uint32_t value;
+    };
+
+    export Swapchain create_swapchain(
+        Device device,
+        VkSwapchainCreateFlagsKHR flags,
+        Min_image_count min_image_count,
+        Surface surface,
+        VkFormat image_format,
+        VkColorSpaceKHR image_color_space,
+        VkExtent2D image_extent,
+        Array_layer_count image_array_layers,
+        VkImageUsageFlags image_usage,
+        VkSharingMode image_sharing_mode,
+        std::span<Queue_family_index const> queue_family_indices,
+        VkSurfaceTransformFlagBitsKHR pre_transform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR,
+        VkCompositeAlphaFlagBitsKHR composite_alpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR,
+        VkPresentModeKHR present_mode = VK_PRESENT_MODE_IMMEDIATE_KHR,
+        bool clipped = false,
+        Swapchain old_swapchain = {},
+        std::optional<Allocation_callbacks> allocator = {}
+    ) noexcept;
+
+    export void destroy_swapchain(
+        Device device,
+        Swapchain swapchain,
+        std::optional<Allocation_callbacks> allocator = {}
+    ) noexcept;
+
+
+    export std::pmr::vector<VkImage> get_swapchain_images(
+        Device device,
+        Swapchain swapchain,
+        std::pmr::polymorphic_allocator<VkSurfaceFormatKHR> allocator = {}
+    ) noexcept;
+
+
+    export struct Swapchain_image_index
+    {
+        std::uint32_t value;
+    };
+
+    export Swapchain_image_index acquire_next_image(
+        Device device,
+        Swapchain swapchain,
+        std::uint64_t timeout,
+        std::optional<Semaphore> semaphore,
+        std::optional<Fence> fence
+    ) noexcept;
+
+
+    export void queue_present(
+        Queue queue,
+        std::span<VkSemaphore const> semaphores_to_wait,
+        Swapchain swapchain,
+        Swapchain_image_index swapchain_image_index
+    ) noexcept;
+}
