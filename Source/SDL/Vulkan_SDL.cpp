9c5e197c1261e75abbec28b01c4f6eb533a5e235
João Maia <jpmmaia@gmail.com>
2020-02-09 00:08:48 +0200

    Add Vulkan_SDL module

---
+++ Source/SDL/Vulkan_SDL.cpp
@@ -0,0 +1,68 @@
+module maia.sdl.vulkan;
+
+import <SDL2/SDL.h>;
+import <SDL2/SDL_vulkan.h>;
+
+import <vulkan/vulkan.h>;
+
+import <cassert>;
+import <iostream>;
+import <memory_resource>;
+import <vector>;
+
+namespace Maia::SDL::Vulkan
+{
+    namespace
+    {
+        void check(SDL_bool const status) noexcept
+        {
+            if (status != SDL_TRUE)
+            {
+                std::cerr << SDL_GetError() << '\n';
+            }
+
+            assert(status == SDL_TRUE);
+        }
+    }
+
+    std::pmr::vector<const char*> get_sdl_required_instance_extensions(
+        SDL_Window& window,
+        std::pmr::polymorphic_allocator<const char*> allocator
+    ) noexcept
+    {
+        unsigned int count = 0;
+        check(
+            SDL_Vulkan_GetInstanceExtensions(&window, &count, nullptr));
+
+        std::pmr::vector<const char*> instance_extensions{allocator};
+        instance_extensions.resize(count);
+
+        check(
+            SDL_Vulkan_GetInstanceExtensions(&window, &count, instance_extensions.data()));
+
+        return instance_extensions;
+    }
+
+    VkSurfaceKHR create_surface(
+        SDL_Window& window,
+        VkInstance const instance
+    ) noexcept
+    {
+        VkSurfaceKHR surface = {};
+        check(
+            SDL_Vulkan_CreateSurface(&window, instance, &surface));
+
+        return surface;
+    }
+
+    Surface_extent get_surface_extent(
+        SDL_Window& window
+    ) noexcept
+    {        
+        int width = 0;
+        int height = 0;
+        SDL_Vulkan_GetDrawableSize(&window, &width, &height);
+
+        return {width, height};
+    }
+}
---
+++ Source/SDL/Vulkan_SDL.cxx
@@ -0,0 +1,31 @@
+export module maia.sdl.vulkan;
+
+import <SDL2/SDL.h>;
+
+import <vulkan/vulkan.h>;
+
+import <memory_resource>;
+import <vector>;
+
+namespace Maia::SDL::Vulkan
+{
+    export std::pmr::vector<const char*> get_sdl_required_instance_extensions(
+        SDL_Window& window,
+        std::pmr::polymorphic_allocator<const char*> allocator = {}
+    ) noexcept;
+
+    export struct Surface_extent
+    {
+        int width;
+        int height;
+    };
+
+    export VkSurfaceKHR create_surface(
+        SDL_Window& window,
+        VkInstance const instance
+    ) noexcept;
+
+    export Surface_extent get_surface_extent(
+        SDL_Window& window
+    ) noexcept;
+}
