223b71771eba0f500b20ea7142728d0e19bf09bf
João Maia <jpmmaia@gmail.com>
2020-02-09 00:11:28 +0200

    Export more modules through maia.renderer.vulkan

--- Internal/Renderer/Source/Vulkan/Module.cxx
+++ Internal/Renderer/Source/Vulkan/Module.cxx
@@ -1,6 +1,14 @@
 export module maia.renderer.vulkan;
 
+export import maia.renderer.vulkan.allocation_callbacks;
 export import maia.renderer.vulkan.check;
+export import maia.renderer.vulkan.command_buffer;
+export import maia.renderer.vulkan.command_pool;
 export import maia.renderer.vulkan.device;
+export import maia.renderer.vulkan.device_memory;
+export import maia.renderer.vulkan.fence;
+export import maia.renderer.vulkan.image;
 export import maia.renderer.vulkan.instance;
 export import maia.renderer.vulkan.physical_device;
+export import maia.renderer.vulkan.queue;
+export import maia.renderer.vulkan.render_pass;
