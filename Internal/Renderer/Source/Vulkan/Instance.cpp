7130c9af7ef315d490d35cea6009c76d8f9af030
João Maia <jpmmaia@gmail.com>
2020-02-29 10:12:52 +0200

    Improve create instance

--- Internal/Renderer/Source/Vulkan/Instance.cpp
+++ Internal/Renderer/Source/Vulkan/Instance.cpp
@@ -7,7 +7,9 @@ import <vulkan/vulkan.h>;
 import <cassert>;
 import <ostream>;
 import <memory_resource>;
+import <optional>;
 import <span>;
+import <string_view>;
 
 namespace Maia::Renderer::Vulkan
 {
@@ -37,17 +39,41 @@ namespace Maia::Renderer::Vulkan
         return layer_properties;
     }
 
-    Instance create_instance(std::span<char const* const> const enabled_layers, std::span<char const* const> const enabled_extensions) noexcept
+    API_version make_api_version(std::uint16_t const major, std::uint16_t const minor, std::uint16_t const patch) noexcept
     {
-        VkInstanceCreateInfo info = {};
-        info.sType = VkStructureType::VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-        info.pNext = nullptr;
-        info.flags = {};
-        info.pApplicationInfo = nullptr;
-        info.enabledLayerCount = enabled_layers.size();
-        info.ppEnabledLayerNames = enabled_layers.data();
-        info.enabledExtensionCount = enabled_extensions.size();
-        info.ppEnabledExtensionNames = enabled_extensions.data();
+        return {static_cast<std::uint32_t>(VK_MAKE_VERSION(major, minor, patch))};
+    }
+
+    Instance create_instance(
+        std::optional<Application_description> const application_description,
+        std::optional<Engine_description> const engine_description,
+        API_version const api_version,
+        std::span<char const* const> const enabled_layers,
+        std::span<char const* const> const enabled_extensions,
+        void const* const next) noexcept
+    {
+        VkApplicationInfo const application_info
+        {
+            .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+            .pNext = next,
+            .pApplicationName = application_description ? application_description->name.data() : nullptr,
+            .applicationVersion = application_description ? application_description->version : 0,
+            .pEngineName = engine_description ? engine_description->name.data() : nullptr,
+            .engineVersion = engine_description ? engine_description->version : 0,
+            .apiVersion = api_version.value,
+        };
+
+        VkInstanceCreateInfo const info
+        {
+            .sType = VkStructureType::VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+            .pNext = nullptr,
+            .flags = {},
+            .pApplicationInfo = &application_info,
+            .enabledLayerCount = static_cast<std::uint32_t>(enabled_layers.size()),
+            .ppEnabledLayerNames = enabled_layers.data(),
+            .enabledExtensionCount = static_cast<std::uint32_t>(enabled_extensions.size()),
+            .ppEnabledExtensionNames = enabled_extensions.data(),
+        };
 
         VkInstance instance = {};
         check_result(
--- Internal/Renderer/Source/Vulkan/Instance.cxx
+++ Internal/Renderer/Source/Vulkan/Instance.cxx
@@ -2,9 +2,12 @@ export module maia.renderer.vulkan.instance;
 
 import <vulkan/vulkan.h>;
 
-import <memory_resource>;
+import <cstdint>;
 import <iosfwd>;
+import <memory_resource>;
+import <optional>;
 import <span>;
+import <string_view>;
 import <vector>;
 
 namespace Maia::Renderer::Vulkan
@@ -13,10 +16,35 @@ namespace Maia::Renderer::Vulkan
 
     export std::pmr::vector<VkLayerProperties> enumerate_instance_layer_properties(std::pmr::polymorphic_allocator<VkLayerProperties> const& allocator = {}) noexcept;
 
+    export struct Application_description
+    {
+        std::string_view name;
+        std::uint32_t version;
+    };
+
+    export struct Engine_description
+    {
+        std::string_view name;
+        std::uint32_t version;
+    };
+
+    export struct API_version
+    {
+        std::uint32_t value;
+    };
+
+    export API_version make_api_version(std::uint16_t major, std::uint16_t minor, std::uint16_t patch) noexcept;
+
     export struct Instance
     {
         VkInstance value;
     };
 
-    export Instance create_instance(std::span<char const* const> const enabled_layers, std::span<char const* const> enabled_extensions) noexcept;
+    export Instance create_instance(
+        std::optional<Application_description> application_description,
+        std::optional<Engine_description> engine_description,
+        API_version api_version,
+        std::span<char const* const> enabled_layers,
+        std::span<char const* const> enabled_extensions,
+        void const* next = nullptr) noexcept;
 }
