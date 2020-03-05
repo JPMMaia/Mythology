5633e0da0ec39cd203e9614c39d5fb6c8e00f01c
João Maia <jpmmaia@gmail.com>
2020-03-05 21:53:21 +0200

    Destroy resources when exiting Windowless app

--- Source/Windowless/Windowless.cpp
+++ Source/Windowless/Windowless.cpp
@@ -15,13 +15,101 @@ import <optional>;
 import <span>;
 import <vector>;
 
+using namespace Maia::Renderer::Vulkan;
+
 namespace Mythology::Windowless
 {
     namespace
     {
-        auto const is_extension_to_enable = [](VkExtensionProperties const& properties) -> bool
+        struct Device_resources
         {
-            return false;
+            explicit Device_resources(API_version const api_version) noexcept
+            {
+                using namespace Mythology::Core::Vulkan;
+
+                this->instance = create_instance(Application_description{"Mythology", 1}, Engine_description{"Mythology Engine", 1}, api_version);
+
+                this->physical_device = select_physical_device(this->instance);
+                
+                this->graphics_queue_family_index = find_graphics_queue_family_index(this->physical_device);
+
+                auto const is_extension_to_enable = [](VkExtensionProperties const& properties) -> bool
+                {
+                    return false;
+                };
+
+                this->device = create_device(this->physical_device, {&this->graphics_queue_family_index, 1}, is_extension_to_enable);
+
+                this->fence = create_fence(this->device, {}, {});
+        
+                this->command_pool = create_command_pool(this->device, VK_COMMAND_POOL_CREATE_TRANSIENT_BIT, this->graphics_queue_family_index, {});
+            }
+            Device_resources(Device_resources const&) = delete;
+            Device_resources(Device_resources&&) = delete;
+            ~Device_resources() noexcept
+            {
+                if (this->command_pool.value != VK_NULL_HANDLE)
+                {
+                    destroy_command_pool(this->device, this->command_pool, {});
+                }
+
+                if (this->fence.value != VK_NULL_HANDLE)
+                {
+                    destroy_fence(this->device, this->fence, {});
+                }
+
+                if (this->device.value != VK_NULL_HANDLE)
+                {
+                    destroy_device(this->device);
+                }
+
+                if (this->instance.value != VK_NULL_HANDLE)
+                {
+                    destroy_instance(this->instance);
+                }
+            }
+
+            Device_resources& operator=(Device_resources const&) = delete;
+            Device_resources& operator=(Device_resources&&) = delete;
+
+            Instance instance = {};
+            Physical_device physical_device = {};
+            Queue_family_index graphics_queue_family_index = {};
+            Device device = {};
+            Fence fence = {};
+            Command_pool command_pool = {};
+        };
+
+        struct Application_resources
+        {
+            explicit Application_resources(
+                Physical_device const physical_device,
+                Device const device,
+                VkExtent3D const color_image_extent) noexcept :
+                device{device},
+                device_memory_and_color_image(Mythology::Core::Vulkan::create_device_memory_and_color_image(physical_device, device, VK_FORMAT_R8G8B8A8_UINT, color_image_extent))
+            {
+            }
+            Application_resources(Application_resources const&) = delete;
+            Application_resources(Application_resources&&) = delete;
+            ~Application_resources() noexcept
+            {
+                if (this->device_memory_and_color_image.color_image.value != VK_NULL_HANDLE)
+                {
+                    destroy_image(this->device, this->device_memory_and_color_image.color_image, {});
+                }
+
+                if (this->device_memory_and_color_image.device_memory.value != VK_NULL_HANDLE)
+                {
+                    free_memory(this->device, this->device_memory_and_color_image.device_memory, {});
+                }
+            }
+
+            Application_resources& operator=(Application_resources const&) = delete;
+            Application_resources& operator=(Application_resources&&) = delete;
+
+            Device device;
+            Mythology::Core::Vulkan::Device_memory_and_color_image device_memory_and_color_image;
         };
     }
 
@@ -32,18 +120,19 @@ namespace Mythology::Windowless
         using namespace Mythology::Core::Vulkan;
         using namespace Maia::Renderer::Vulkan;
 
-        API_version const api_version{make_api_version(1, 2, 0)};
-        Instance const instance = create_instance(Application_description{"Mythology", 1}, Engine_description{"Mythology Engine", 1}, api_version);
-        Physical_device const physical_device = select_physical_device(instance);
-        Queue_family_index const graphics_queue_family_index = find_graphics_queue_family_index(physical_device);
-        Device const device = create_device(physical_device, {&graphics_queue_family_index, 1}, is_extension_to_enable);
-        Command_pool const command_pool = create_command_pool(device, VK_COMMAND_POOL_CREATE_TRANSIENT_BIT, graphics_queue_family_index, {});
+        Device_resources const device_resources{make_api_version(1, 2, 0)};
+        Physical_device const physical_device = device_resources.physical_device;
+        Device const device = device_resources.device;
+        Queue_family_index const graphics_queue_family_index = device_resources.graphics_queue_family_index;
+        Fence const fence = device_resources.fence;
+        Command_pool const command_pool = device_resources.command_pool;
+        
         Queue const queue = get_device_queue(device, graphics_queue_family_index, 0);
-        Fence const fence = create_fence(device, {}, {});
-
+        
         VkExtent3D constexpr color_image_extent{16, 16, 1};
-        Device_memory_and_color_image const device_memory_and_color_image = 
-            create_device_memory_and_color_image(physical_device, device, VK_FORMAT_R8G8B8A8_UINT, color_image_extent);
+        Application_resources const application_resources{physical_device, device, color_image_extent};
+        Device_memory const color_image_device_memory = application_resources.device_memory_and_color_image.device_memory;
+        Image const color_image = application_resources.device_memory_and_color_image.color_image;
 
         {
             std::pmr::vector<Command_buffer> const command_buffers = 
@@ -63,7 +152,7 @@ namespace Mythology::Windowless
                     .uint32 = {0, 128, 0, 255}
                 };
 
-                render(command_buffer, device_memory_and_color_image.color_image, clear_color);
+                render(command_buffer, color_image, clear_color);
             }
             end_command_buffer(command_buffer);
 
@@ -75,12 +164,12 @@ namespace Mythology::Windowless
         {
             VkSubresourceLayout const color_image_layout = get_subresource_layout(
                 device,
-                device_memory_and_color_image.color_image,
+                color_image,
                 {.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT, .mipLevel = 0, .arrayLayer = 0}
             );
 
             std::pmr::vector<std::byte> const color_image_data = 
-                read_memory(device, device_memory_and_color_image.device_memory, color_image_layout);
+                read_memory(device, color_image_device_memory, color_image_layout);
 
             std::ofstream output_file{output_filename};
             write_p3(output_file, color_image_data, color_image_layout, color_image_extent);
