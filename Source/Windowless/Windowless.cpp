55e5acde194b68f97827fc08b3b883c6af213525
João Maia <jpmmaia@gmail.com>
2020-03-28 15:39:18 +0200

    Update windowless app

--- Source/Windowless/Windowless.cpp
+++ Source/Windowless/Windowless.cpp
@@ -1,5 +1,6 @@
 module mythology.windowless;
 
+import mythology.core.utilities;
 import mythology.core.vulkan;
 import maia.renderer.vulkan;
 
@@ -16,6 +17,8 @@ import <span>;
 import <vector>;
 
 using namespace Maia::Renderer::Vulkan;
+using namespace Mythology::Core;
+using namespace Mythology::Core::Vulkan;
 
 namespace Mythology::Windowless
 {
@@ -83,17 +86,61 @@ namespace Mythology::Windowless
         struct Application_resources
         {
             explicit Application_resources(
+                std::filesystem::path const shaders_path,
                 Physical_device const physical_device,
                 Device const device,
+                VkFormat const color_image_format,
                 VkExtent3D const color_image_extent) noexcept :
                 device{device},
-                device_memory_and_color_image(Mythology::Core::Vulkan::create_device_memory_and_color_image(physical_device, device, VK_FORMAT_R8G8B8A8_UINT, color_image_extent))
+                device_memory_and_color_image(Mythology::Core::Vulkan::create_device_memory_and_color_image(physical_device, device, color_image_format, color_image_extent)),
+                pipeline_layout{create_pipeline_layout(device, {}, {})},
+                render_pass{create_render_pass(device, color_image_format)},
+                triangle_vertex_shader_module{create_shader_module(device, {}, convert_bytes<std::uint32_t>(read_bytes(shaders_path / "Triangle.vertex.spv")))},
+                white_fragment_shader_module{create_shader_module(device, {}, convert_bytes<std::uint32_t>(read_bytes(shaders_path / "White.fragment.spv")))},
+                white_triangle_pipeline{create_vertex_and_fragment_pipeline(device, {}, pipeline_layout, render_pass.value, 0, 1, triangle_vertex_shader_module.value, white_fragment_shader_module.value)},
+                color_image_view{create_image_view(device, {}, device_memory_and_color_image.color_image, VK_IMAGE_VIEW_TYPE_2D, color_image_format, {}, {.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT, .baseMipLevel = 0, .levelCount = 1, .baseArrayLayer = 0, .layerCount = 1}, {})},
+                framebuffer{create_framebuffer(device, {}, render_pass, {&color_image_view.value, 1}, {color_image_extent.width, color_image_extent.height, 1}, {})}
             {
             }
             Application_resources(Application_resources const&) = delete;
             Application_resources(Application_resources&&) = delete;
             ~Application_resources() noexcept
             {
+                if (color_image_view.value != VK_NULL_HANDLE)
+                {
+                    destroy_image_view(device, color_image_view, {});
+                }
+
+                if (framebuffer.value != VK_NULL_HANDLE)
+                {
+                    destroy_framebuffer(device, framebuffer, {});
+                }
+
+                if (white_triangle_pipeline != VK_NULL_HANDLE)
+                {
+                    vkDestroyPipeline(device.value, white_triangle_pipeline, nullptr);
+                }
+
+                if (triangle_vertex_shader_module.value != VK_NULL_HANDLE)
+                {
+                    destroy_shader_module(device, triangle_vertex_shader_module);
+                }
+
+                if (white_fragment_shader_module.value != VK_NULL_HANDLE)
+                {
+                    destroy_shader_module(device, white_fragment_shader_module);
+                }
+
+                if (render_pass.value != VK_NULL_HANDLE)
+                {
+                    destroy_render_pass(device, render_pass, {});
+                }
+
+                if (pipeline_layout != VK_NULL_HANDLE)
+                {
+                    destroy_pipeline_layout(device, pipeline_layout, {});
+                }
+
                 if (this->device_memory_and_color_image.color_image.value != VK_NULL_HANDLE)
                 {
                     destroy_image(this->device, this->device_memory_and_color_image.color_image, {});
@@ -110,6 +157,13 @@ namespace Mythology::Windowless
 
             Device device;
             Mythology::Core::Vulkan::Device_memory_and_color_image device_memory_and_color_image;
+            VkPipelineLayout pipeline_layout;
+            Render_pass render_pass;
+            Shader_module triangle_vertex_shader_module;
+            Shader_module white_fragment_shader_module;
+            VkPipeline white_triangle_pipeline;
+            Image_view color_image_view;
+            Framebuffer framebuffer;
         };
     }
 
@@ -119,6 +173,8 @@ namespace Mythology::Windowless
     {
         using namespace Mythology::Core::Vulkan;
         using namespace Maia::Renderer::Vulkan;
+        
+        std::filesystem::path const shaders_path = std::filesystem::current_path() / "../shaders";
 
         Device_resources const device_resources{make_api_version(1, 2, 0)};
         Physical_device const physical_device = device_resources.physical_device;
@@ -129,10 +185,14 @@ namespace Mythology::Windowless
         
         Queue const queue = get_device_queue(device, graphics_queue_family_index, 0);
         
+        VkFormat constexpr color_image_format = VK_FORMAT_R8G8B8A8_UINT;
         VkExtent3D constexpr color_image_extent{16, 16, 1};
-        Application_resources const application_resources{physical_device, device, color_image_extent};
+        Application_resources const application_resources{shaders_path, physical_device, device, color_image_format, color_image_extent};
         Device_memory const color_image_device_memory = application_resources.device_memory_and_color_image.device_memory;
         Image const color_image = application_resources.device_memory_and_color_image.color_image;
+        Render_pass const render_pass = application_resources.render_pass;
+        Framebuffer const framebuffer = application_resources.framebuffer;
+        VkPipeline const white_triangle_pipeline = application_resources.white_triangle_pipeline;
 
         {
             std::pmr::vector<Command_buffer> const command_buffers = 
@@ -148,11 +208,18 @@ namespace Mythology::Windowless
 
             begin_command_buffer(command_buffer, VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT, {});
             {
-                VkClearColorValue const clear_color = {
+                VkClearColorValue const clear_color =
+                {
                     .uint32 = {0, 128, 0, 255}
                 };
 
-                render(command_buffer, color_image, clear_color);
+                VkRect2D const output_render_area
+                {
+                    .offset = {0, 0},
+                    .extent = {color_image_extent.width, color_image_extent.height}
+                };
+
+                render(command_buffer, render_pass, framebuffer, clear_color, white_triangle_pipeline, color_image, output_render_area);
             }
             end_command_buffer(command_buffer);
 
@@ -173,6 +240,6 @@ namespace Mythology::Windowless
 
             std::ofstream output_file{output_filename};
             write_p3(output_file, color_image_data, color_image_layout, color_image_extent);
-        }
+        }        
     }
 }
