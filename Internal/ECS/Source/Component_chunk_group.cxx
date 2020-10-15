185f5fa1cca6a7e682d5dc4080a029269cf7e3ed
João Maia <jpmmaia@gmail.com>
2020-10-15 18:23:39 +0300

    Add Component_chunk_group

---
+++ Internal/ECS/Source/Component_chunk_group.cxx
@@ -0,0 +1,9 @@
+export module maia.ecs.component_chunk_group;
+
+namespace Maia::ECS
+{
+    class Component_chunk_group
+    {
+
+    };
+}
---
+++ Internal/ECS/Test/UnitTest/Component_chunk_group.test.cpp
@@ -0,0 +1,10 @@
+import maia.ecs.component_chunk_group;
+
+import <catch2/catch.hpp>;
+
+namespace Maia::ECS::Test
+{
+    TEST_CASE("Use component chunk group")
+    {
+    }
+}
