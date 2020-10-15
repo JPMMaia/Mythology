2eb513087146b6993fea56906c8fd4a95fe77fa5
João Maia <jpmmaia@gmail.com>
2020-10-15 18:40:18 +0300

    Add component chunk tests

--- Internal/ECS/Source/Component_chunk_group.cxx
+++ Internal/ECS/Source/Component_chunk_group.cxx
@@ -1,9 +1,34 @@
 export module maia.ecs.component_chunk_group;
 
+import maia.ecs.component;
+import maia.ecs.entity;
+
 namespace Maia::ECS
 {
-    class Component_chunk_group
+    export class Component_chunk_group
     {
+    public:
 
+        void add_entity(Entity const entity)
+        {
+        }
+
+        template<Concept::Component Component_t>
+        Component_t get_component_value() noexcept
+        {
+            return {};
+        }
+    
+        template<Concept::Component Component_t>
+        bool has_component() noexcept
+        {
+            return false;
+        }
     };
+
+    export template<Concept::Component... Component_t>
+    Component_chunk_group create_component_chunk_group()
+    {
+        return {};
+    }
 }
--- Internal/ECS/Test/UnitTest/Component_chunk_group.test.cpp
+++ Internal/ECS/Test/UnitTest/Component_chunk_group.test.cpp
@@ -1,10 +1,73 @@
+import maia.ecs.component;
 import maia.ecs.component_chunk_group;
+import maia.ecs.entity;
 
 import <catch2/catch.hpp>;
 
 namespace Maia::ECS::Test
 {
+    namespace
+    {
+        template<typename T>
+        struct Component_base {};
+
+        template<typename T>
+        bool operator==(Component_base<T> const lhs, Component_base<T> const rhs)
+		{
+            return std::memcmp(&lhs, &rhs, sizeof(T));
+        }
+
+        template<typename T>
+        bool operator!=(Component_base<T> const lhs, Component_base<T> const rhs)
+		{
+            return !(lhs == rhs);
+        }
+
+        struct Component_a : Component_base<Component_a>
+        {
+            int value = 1;
+        };
+
+        struct Component_b : Component_base<Component_b>
+        {
+            int value = 2;
+        };
+
+        struct Component_c : Component_base<Component_c>
+        {
+            int value = 3;
+        };
+    }
+
     TEST_CASE("Use component chunk group")
     {
+        SECTION("Component chunk group hides the component types")
+        {
+            Component_chunk_group group = create_component_chunk_group<Component_a, Component_b>();
+
+            CHECK(group.has_component<Component_a>());
+            CHECK(group.has_component<Component_b>());
+            CHECK(!group.has_component<Component_c>());
+        }
+
+        SECTION("Components are initialized with default values")
+        {
+            Component_chunk_group group = create_component_chunk_group<Component_a, Component_b>();
+            group.add_entity(Entity{1});
+
+            {
+                constexpr Component_a expected_value{};
+                Component_a const actual_value = group.get_component_value<Component_a>();
+
+                CHECK(actual_value == expected_value);
+            }
+
+            {
+                constexpr Component_b expected_value{};
+                Component_b const actual_value = group.get_component_value<Component_b>();
+
+                CHECK(actual_value == expected_value);
+            }
+        }
     }
 }
