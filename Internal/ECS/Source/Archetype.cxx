5e5f0b6ec5a2194cb807286d8f61a6add3f2bdbd
João Maia <jpmmaia@gmail.com>
2020-10-17 15:09:03 +0300

    Implement archetype

--- Internal/ECS/Source/Archetype.cxx
+++ Internal/ECS/Source/Archetype.cxx
@@ -4,6 +4,7 @@ import maia.ecs.component;
 import maia.ecs.shared_component;
 
 import <bitset>;
+import <cassert>;
 import <cstddef>;
 import <cstdint>;
 import <optional>;
@@ -14,35 +15,54 @@ namespace Maia::ECS
 {
     export struct Archetype
     {
-        bool has_component(Component_type_ID const component_id) const noexcept
+        bool has_component(Component_type_ID const component_type_id) const noexcept
         {
-            return true;
+            assert(component_type_id.value < this->component_type_ids.size());
+
+            return this->component_type_ids.test(component_type_id.value);
         }
 
         bool has_shared_component() const noexcept
         {
-            return true;
+            return this->shared_component_type_id.has_value();
         }
 
-        bool has_shared_component(Shared_component_type_ID const shared_component_id) const noexcept
+        bool has_shared_component(Shared_component_type_ID const shared_component_type_id) const noexcept
         {
-            return true;
+            return this->shared_component_type_id.has_value() ?
+                *this->shared_component_type_id == shared_component_type_id :
+                false;
         }
+
+        std::bitset<496> component_type_ids;
+        std::optional<Shared_component_type_ID> shared_component_type_id;
     };
 
-    export Archetype create_archetype(std::span<Component_type_ID const> component_ids) noexcept
+    export Archetype create_archetype(std::span<Component_type_ID const> const component_type_ids) noexcept
     {
-        return {};
+        Archetype archetype{};
+        
+        for (Component_type_ID const component_type_id : component_type_ids)
+        {
+            assert(component_type_id.value < archetype.component_type_ids.size());
+
+            archetype.component_type_ids.set(component_type_id.value);
+        }
+
+        return archetype;
     }
 
-    export Archetype create_archetype(Shared_component_type_ID const shared_component_id, std::span<Component_type_ID const> component_ids) noexcept
+    export Archetype create_archetype(Shared_component_type_ID const shared_component_type_id, std::span<Component_type_ID const> const component_type_ids) noexcept
     {
-        return {};
+        Archetype archetype = create_archetype(component_type_ids);
+        archetype.shared_component_type_id = shared_component_type_id;
+
+        return archetype;
     }
 
     export bool operator==(Archetype const& lhs, Archetype const& rhs) noexcept
     {
-        return true;
+        return lhs.component_type_ids == rhs.component_type_ids;
     }
 
     export bool operator!=(Archetype const& lhs, Archetype const& rhs) noexcept
