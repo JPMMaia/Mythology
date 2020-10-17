b199aab743c7688a76a2a470485b6613eefbbffa
João Maia <jpmmaia@gmail.com>
2020-10-17 15:52:15 +0300

    Implement a part of Entity manager

---
+++ Internal/ECS/Source/Entity_manager.cpp
@@ -0,0 +1,15 @@
+module maia.ecs.entity_manager;
+
+import maia.ecs.archetype;
+import maia.ecs.component;
+import maia.ecs.component_chunk_group;
+import maia.ecs.components_view;
+import maia.ecs.entity;
+import maia.ecs.shared_component;
+
+import <cassert>;
+import <vector>;
+
+namespace Maia::ECS
+{
+}
--- Internal/ECS/Source/Entity_manager.cxx
+++ Internal/ECS/Source/Entity_manager.cxx
@@ -2,11 +2,17 @@ export module maia.ecs.entity_manager;
 
 import maia.ecs.archetype;
 import maia.ecs.component;
+import maia.ecs.component_chunk_group;
 import maia.ecs.components_view;
 import maia.ecs.entity;
 import maia.ecs.shared_component;
 
+import <algorithm>;
+import <cassert>;
+import <cstddef>;
+import <optional>;
 import <span>;
+import <vector>;
 
 namespace Maia::ECS
 {
@@ -27,14 +33,32 @@ namespace Maia::ECS
 
         Entity create_entity(Archetype const& archetype)
         {
-            // Precondition: Archetype does not contain a shared component
-            return {};
+            assert(!archetype.has_shared_component());
+
+            std::optional<Archetype_index> const archetype_index = find_archetype_index(archetype);
+
+            if (archetype_index)
+            {
+                Component_chunk_group& chunk_group = m_component_chunk_groups.at(*archetype_index);
+
+                return create_entity(chunk_group);
+            }
+            else
+            {
+                m_archetypes.push_back(archetype);
+                m_component_chunk_groups.push_back(Component_chunk_group{});
+
+                Component_chunk_group& chunk_group = m_component_chunk_groups.back();
+
+                return create_entity(chunk_group);
+            }
         }
 
         template<Concept::Shared_component Shared_component_t>
         Entity create_entity(Archetype const& archetype, Shared_component_t const& shared_component)
         {
-            // Precondition: Archetype contains a shared component
+            assert(archetype.has_shared_component());
+
             return {};
         }
 
@@ -94,5 +118,49 @@ namespace Maia::ECS
         {
             return {};
         }
+
+    private:
+
+        using Archetype_index = std::iterator_traits<std::vector<Archetype>::const_iterator>::difference_type;
+
+        std::optional<Archetype_index> find_archetype_index(Archetype const& archetype) const noexcept
+        {
+            auto const archetype_iterator = std::find(m_archetypes.begin(), m_archetypes.end(), archetype);
+
+            if (archetype_iterator != m_archetypes.end())
+            {
+                return std::distance(m_archetypes.begin(), archetype_iterator);
+            }
+            else
+            {
+                return {};
+            }
+        }
+
+        Entity::Integral_type get_next_entity_value() const noexcept
+        {
+            return m_next_entity_value;
+        }
+
+        void increment_next_entity_value() noexcept
+        {
+            ++m_next_entity_value;
+        }
+
+        Entity create_entity(Component_chunk_group& component_chunk_group)
+        {
+            Entity::Integral_type const new_entity_value = get_next_entity_value();
+            increment_next_entity_value();
+
+            Entity const new_entity{new_entity_value};
+            
+            component_chunk_group.add_entity(new_entity);
+            
+            return new_entity;
+        }
+
+        std::vector<Archetype> m_archetypes;
+        std::vector<Component_chunk_group> m_component_chunk_groups;
+        Entity::Integral_type m_next_entity_value;
     };
 }
