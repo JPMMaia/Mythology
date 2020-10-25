55af350d0de792b11ec0c6ff0316b37380ccc41e
João Maia <jpmmaia@gmail.com>
2020-10-25 17:10:08 +0200

    Add component chunk tests

---
+++ Internal/ECS/Test/UnitTest/Component_chunk.test.cpp
@@ -0,0 +1,11 @@
+import maia.ecs.component_chunk;
+
+import <catch2/catch.hpp>;
+
+namespace Maia::ECS::Test
+{
+    TEST_CASE("", "[Component_chunk]")
+    {
+        CHECK(true == true);
+    }
+}
