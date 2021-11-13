21f93999e44cfae60c947c552be4e3f60fe343aa
João Maia <jpmmaia@gmail.com>
2021-11-13 18:44:34 +0200

    Continue with raytracing changes

--- Tools/Blender-Mythology/ray_tracing.py
+++ Tools/Blender-Mythology/ray_tracing.py
@@ -1,7 +1,17 @@
 import bpy
 import nodeitems_utils
+import typing
 
-from .pipeline_state import PipelineLibraryNodeSocket
+from .common import find_index, get_input_node
+from .pipeline_state import (
+    PipelineLayoutNode,
+    PipelineNode,
+    PipelineShaderStageNode,
+    ShaderModuleNode,
+    create_pipeline_dynamic_state_json,
+    create_pipeline_library_json,
+    create_shader_stages_json,
+)
 from .render_node_tree import RenderTreeNode
 
 
@@ -174,6 +184,7 @@ class RayTracingPipelineStateNode(bpy.types.Node, RenderTreeNode):
 
     bl_label = "Ray Tracing Pipeline"
 
+    name_property: bpy.props.StringProperty(name="Name")
     flags_property: bpy.props.EnumProperty(
         name="Flags",
         items=ray_tracing_pipeline_create_flag_bits,
@@ -181,9 +192,6 @@ class RayTracingPipelineStateNode(bpy.types.Node, RenderTreeNode):
     )
     max_pipeline_ray_recursion_depth_property: bpy.props.IntProperty(
         name="Max Pipeline Ray Recursion Depth Property", default=1
-    )
-    base_pipeline_index_property: bpy.props.IntProperty(
-        name="Base Pipeline Index", default=-1
     )
 
     def init(self, context):
@@ -193,14 +201,143 @@ class RayTracingPipelineStateNode(bpy.types.Node, RenderTreeNode):
 
         self.inputs.new("PipelineLibraryNodeSocket", "Library Info")
         self.inputs.new("RayTracingPipelineInterfaceNodeSocket", "Library Interface")
-        self.inputs.new("DynamicStateNodeSocket", "Dynamic State")
+        self.inputs.new("PipelineDynamicStateNodeSocket", "Dynamic State")
         self.inputs.new("PipelineLayoutNodeSocket", "Layout")
         self.inputs.new("PipelineNodeSocket", "Base Pipeline")
 
-        self.outputs.new("RayTracingPipelineStateNodeSocket", "Pipeline")
+        self.outputs.new("PipelineNodeSocket", "Pipeline")
 
     def draw_buttons(self, context, layout):
-
+        layout.prop(self, "name_property")
         layout.prop(self, "flags_property")
         layout.prop(self, "max_pipeline_ray_recursion_depth_property")
-        layout.prop(self, "base_pipeline_index_property")
+
+
+JSONType = typing.Union[
+    str, int, float, bool, None, typing.Dict[str, typing.Any], typing.List[typing.Any]
+]
+
+
+def gather_ray_tracing_shader_stages(
+    shader_group_nodes: typing.List[RayTracingShaderGroupNode],
+) -> typing.List[PipelineShaderStageNode]:
+
+    shader_stages_per_group = [
+        [
+            input.links[0].from_node if (len(input.links) == 1) else None
+            for input in shader_group.inputs
+            if input.name == "General Shader"
+            or input.name == "Closest Hit Shader"
+            or input.name == "Any Hit Shader"
+            or input.name == "Intersection Shader"
+        ]
+        for shader_group in shader_group_nodes
+    ]
+
+    shader_stages = [
+        shader_stage
+        for shader_stages in shader_stages_per_group
+        for shader_stage in shader_stages
+        if shader_stage != None
+    ]
+
+    return shader_stages
+
+
+def create_ray_tracing_shader_group_json(
+    shader_group_node: RayTracingShaderGroupNode,
+    pipeline_shader_stage_nodes: typing.List[PipelineShaderStageNode],
+) -> JSONType:
+
+    json = {
+        "type": shader_group_node.get("type_property", 0),
+    }
+
+    if len(shader_group_node.inputs["General Shader"].links) == 1:
+        json["general_shader"] = find_index(
+            pipeline_shader_stage_nodes,
+            get_input_node(shader_group_node, "General Shader", 0),
+        )
+
+    if len(shader_group_node.inputs["Closest Hit Shader"].links) == 1:
+        json["closest_hit_shader"] = find_index(
+            pipeline_shader_stage_nodes,
+            get_input_node(shader_group_node, "Closest Hit Shader", 0),
+        )
+
+    if len(shader_group_node.inputs["Any Hit Shader"].links) == 1:
+        json["any_hit_shader"] = find_index(
+            pipeline_shader_stage_nodes,
+            get_input_node(shader_group_node, "Any Hit Shader", 0),
+        )
+
+    if len(shader_group_node.inputs["Intersection Shader"].links) == 1:
+        json["intersection_shader"] = find_index(
+            pipeline_shader_stage_nodes,
+            get_input_node(shader_group_node, "Intersection Shader", 0),
+        )
+
+    return json
+
+
+def create_ray_tracing_pipeline_interface_json(
+    node: RayTracingPipelineInterfaceNode,
+) -> JSONType:
+
+    return {
+        "max_pipeline_ray_payload_size": node.get(
+            "max_pipeline_ray_payload_size_property", 0
+        ),
+        "max_pipeline_ray_hit_attribute_size": node.get(
+            "max_pipeline_ray_hit_attribute_size_property", 0
+        ),
+    }
+
+
+def ray_tracing_pipeline_state_to_json(
+    pipeline_node: RayTracingPipelineStateNode,
+    shader_modules: typing.Tuple[typing.List[ShaderModuleNode], JSONType],
+    pipeline_layouts: typing.Tuple[typing.List[PipelineLayoutNode], JSONType],
+    pipelines: typing.List[PipelineNode],
+) -> JSONType:
+
+    pipeline_shader_stages = gather_ray_tracing_shader_stages(
+        [
+            shader_group_link.from_node
+            for shader_group_link in pipeline_node.inputs["Shader Groups"].links
+        ]
+    )
+
+    json = {
+        "name": pipeline_node.name_property,
+        "flags": pipeline_node.get("stage_flags_property", 0),
+        "stages": create_shader_stages_json(pipeline_shader_stages, shader_modules),
+        "groups": [
+            create_ray_tracing_shader_group_json(
+                group_link.from_node, pipeline_shader_stages
+            )
+            for group_link in pipeline_node.inputs["Shader Groups"].links
+        ],
+        "max_pipeline_ray_recursion_depth": pipeline_node.get(
+            "max_pipeline_ray_recursion_depth_property", 0
+        ),
+        "library_info": create_pipeline_library_json(
+            pipeline_node.inputs["Library Info"].links[0].from_node
+        ),
+        "library_interface": create_ray_tracing_pipeline_interface_json(
+            pipeline_node.inputs["Library Interface"].links[0].from_node
+        ),
+        "dynamic_state": create_pipeline_dynamic_state_json(
+            pipeline_node.inputs["Dynamic State"]
+        ),
+        "layout": find_index(
+            pipeline_layouts, pipeline_node.inputs["Layout"].links[0].from_node
+        ),
+    }
+
+    if len(pipeline_node.inputs["Base Pipeline"].links) == 1:
+        json["base_pipeline"] = find_index(
+            pipelines, pipeline_node.inputs["Base Pipeline"].links[0].from_node
+        )
+
+    return json
